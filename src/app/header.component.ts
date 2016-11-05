076ca6d6ce6fcf0d253228b8c0f33a566573bd1f
GoDotDotDot <chukui0202@gmail.com>
2016-11-05 21:34:27 +0800

    2016-11-05 添加响应式

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -7,5 +7,14 @@ import {Component} from '@angular/core'
 }
 )
 export class SupermapHeaderComponent{
-
+  private __SHOWMENU:boolean;
+  private display:string;
+  constructor(){
+     this.__SHOWMENU=false;
+  }
+  showMenu():void{
+    this.__SHOWMENU===true ?this.display='none':this.display='block'
+    document.getElementById("menu").style.display=this.display;
+    this.__SHOWMENU=!this.__SHOWMENU;
+  }
 }
