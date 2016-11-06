520803c7e8eab137bef82a694d46dfd9101ce1d5
GoDotDotDot <chukui0202@gmail.com>
2016-11-06 14:12:59 +0800

    2016-11-06

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -13,8 +13,8 @@ export class SupermapHeaderComponent{
      this.__SHOWMENU=false;
   }
   showMenu():void{
-    this.__SHOWMENU===true ?this.display='none':this.display='block'
-    document.getElementById("menu").style.display=this.display;
+    this.__SHOWMENU===true ?this.display='header-right':this.display='header-right header-ul'
+    document.getElementById("menu").className=this.display;
     this.__SHOWMENU=!this.__SHOWMENU;
   }
 }
